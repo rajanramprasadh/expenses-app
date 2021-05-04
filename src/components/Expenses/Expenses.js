8397a4a6f4fd9c44e81b420c5de7fed2d3ab3c1c
rajanramprasadh <rajanramprasadh@gmail.com>
2021-05-04 17:10:50 +0530

    Completed till working with state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,11 @@
-import ExpenseItem from "./ExpenseItem";
+import React from "react";
+import ExpenseItem from "./ExpensItem/ExpenseItem";
+import Card from "../UI/Card";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         id={props.items[0].id}
         title={props.items[0].title}
@@ -28,7 +30,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
